354fac25c45eea0522b9f3830027c07e8c216923
Faith Atieno <atienof44@gmail.com>
2023-06-16 14:11:13 -0700

    Five numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Print all single digit numbers
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = o;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		++digit;
+	}
+	printf("\n");
+	return (0);
+}
