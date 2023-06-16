30a5c71634363163c3bd6da5e00e5cb5cf635ab0
Faith Atieno <atienof44@gmail.com>
2023-06-16 13:47:13 -0700

    Four alphabets

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -8,11 +8,13 @@ int main(void)
 {
 	char ch = 'a';
 
-	while (ch <= 'z') {
-		if (ch == 'e'|| ch == 'q')
+	while (ch <= 'z') 
+	{
+		if (ch == 'e' || ch == 'q')
 			++ch;
 		putchar(ch);
-		++ch; }
+		++ch; 
+	}
 	putchar('\n');
 	return (0);
 }
