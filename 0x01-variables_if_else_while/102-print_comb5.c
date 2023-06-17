92f550a4a898ce0f1eb7638e299eb3a66207694f
Faith Atieno <atienof44@gmail.com>
2023-06-17 12:10:25 -0700

    Combination

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,16 +11,16 @@ int main(void)
 	while (num1 <= 99)
 	{
 		num2 = 0;
-		while (mum2 <= 99)
+		while (num2 <= 99)
 		{
-			if (num2 = num1)
+			if (num2 != num1)
 			{
 				putchar((num1 / 10) + 48);
 				putchar((num1 % 10) + 48);
 				putchar(' ');
 				putchar((num2 / 10) + 48);
 				putchar((num2 % 10) + 48);
-				if (num1 != 98 || num2 != 99)
+				if (num1 != 98)
 				{
 					putchar(',');
 					putchar(' ');
