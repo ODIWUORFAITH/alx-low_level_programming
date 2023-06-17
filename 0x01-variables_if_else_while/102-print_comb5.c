ae464f1330b4e49c14370f28db5e22f7aa621e30
Faith Atieno <atienof44@gmail.com>
2023-06-17 12:04:41 -0700

    Combination

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: Program that prints combinations.
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int num1 = 0, num2;
+	
+	while (num1 <= 99)
+	{
+		num2 = 0;
+		while (mum2 <= 99)
+		{
+			if (num2 = num1)
+			{
+				putchar((num1 / 10) + 48);
+				putchar((num1 % 10) + 48);
+				putchar(' ');
+				putchar((num2 / 10) + 48);
+				putchar((num2 % 10) + 48);
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			++num2;
+		}
+		++num1;
+	}
+	putchar('\n');
+	return (0);
+}
