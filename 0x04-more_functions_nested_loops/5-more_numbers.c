9a132fdcd829d28fc2c002ce2f4e64303bd40e7d
Faith Atieno <atienof44@gmail.com>
2023-06-22 11:23:43 -0700

    5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,16 +7,16 @@ void more _numbers(void)
 {
 	int x, y;
 
-	 for (x = 0; x < 10; x++)
-                {
-                        for (y = 0; y <= 14; y++)
-                        {
-                                if (y > 9)
-                                {
-                                        _putchar((y / 10) + '0');
-                                }
-                                _putchar((y % 10) + '0');
-                        }
-                        _putchar('\n');
-                }
+	for (x = 0; x < 10; x++)
+	{
+		for (y = 0; y <= 14; y++)
+		{
+			if (y > 9)
+			{
+				_putchar((y / 10) + '0');
+			}
+			_putchar((y % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
