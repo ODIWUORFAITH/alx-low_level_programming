31512872d63ba1db0e94e651b2d8a0b4e981c8d3
Faith Atieno <atienof44@gmail.com>
2023-06-22 13:19:56 -0700

    100-prime_factor.c

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,22 +1,32 @@
 #include <stdio.h>
 #include <math.h>
+
 /*
  * main - Prints the largest prime factor of the number 612852475143
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	long x, maxf;
-	long number = 612852475143;
-	double square = sqrt(number);
+    long x, maxf;
+    long number = 612852475143;
+    double square = sqrt(number);
+    
+    maxf = 1; // Initialize maxf to a minimum value
 
-	for (x = 1; x <= square; x++)
-	{
-		if (number % x == 0)
-		{
-			maxf = number / x;
-		}
-	}
-	printf("%ld\n", maxf);
-	return (0);
+    for (x = 2; x <= square; x++)
+    {
+        while (number % x == 0)
+        {
+            maxf = x;
+            number = number / x;
+        }
+    }
+
+    if (number > maxf) // Check if the remaining number is greater than the current max factor
+    {
+        maxf = number;
+    }
+
+    printf("%ld\n", maxf);
+    return 0;
 }
