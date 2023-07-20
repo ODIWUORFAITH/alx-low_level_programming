f3ffa7070647e0f76e0540fcd8fde299d75f8e92
Faith Atieno <atienof44@gmail.com>
2023-07-20 06:46:42 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -5,9 +5,6 @@
  * print_numbers - Prints numbers followed by a new line.
  * @separator: The string to be printed between numbers.
  * @n: The number of integers passed to the function.
- *        If n is 0, the function will only print a new line.
- *        If n is greater than 0 but no integers are provided,
- *        the function will print the separator and then a new line.
  * @...: A variable number of integer arguments to be printed.
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
