c559c3621126bd3f0f7324defacda582f7f8c9f1
Faith Atieno <atienof44@gmail.com>
2023-07-20 05:17:58 -0700

    1-print_numbers.c

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -15,12 +15,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
     unsigned int i;
 
     va_start(ap, n);
-
     for (i = 0; i < n; i++)
     {
         int num = va_arg(ap, int);
         printf("%d", num);
-
         if (i < n - 1 && separator != NULL)
             printf("%s", separator);
     }
