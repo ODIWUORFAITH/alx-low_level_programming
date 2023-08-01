4ecc5caadfac78b45a217797df7bc6aecf206675
Faith Atieno <atienof44@gmail.com>
2023-08-01 05:38:05 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,7 +3,7 @@
 
 /**
  * print_listint - prints all the elements of a listint_t
- * @n: A pointer to the ehad of the list_t list
+ * @h: A pointer to the head of the list_t list
  * Return: The number of nodes in the list_t list
  */
 size_t print_listint(const listint_t *h)
