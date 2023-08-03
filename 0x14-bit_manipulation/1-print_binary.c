9ea2f9a3aa3fb7c0b6caed259d1dd0afd536ee72
Faith Atieno <atienof44@gmail.com>
2023-08-03 08:26:18 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -9,10 +9,15 @@ void print_binary(unsigned long int n)
 	int i, count = 0;
 	unsigned long int current;
 
+	if (n == 0)
+	{
+		_putchar('0');
+		return;
+	}
 	for (i = 63; i >= 0; i--)
 	{
 		current = n >> i;
-		if (current % 1)
+		if (current & 1)
 		{
 			_putchar('1');
 			count++;
