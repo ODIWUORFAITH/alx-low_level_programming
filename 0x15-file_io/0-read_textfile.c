69733d70320fa3219a4fe23bc8fa60aaffe395df
Faith Atieno <atienof44@gmail.com>
2023-08-07 05:08:53 -0700

    0-read_textfile.c

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+#include <unistd.h>
+
+/**
+ * read_textfile - Read text file and print to STDOUT
+ * @filename: Text file being read
+ * @letters: Number of letters to be read
+ * Return: w - actual number of bytes read and printed, 0 when function fails
+ * or filename is NULL
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	char *buf;
+	ssize_t fd, t, w, total_written = 0;
+
+	if (filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	while ((size_t)total_written < letters)
+	{
+		t = read(fd, buf, letters - total_written);
+		if (t == 0)
+			break;
+		else if (t == -1)
+		{
+			free(buf);
+			close(fd);
+			return (0);
+		}
+		w = write(STDOUT_FILENO, buf, t);
+		if (w == -1 || w != t)
+		{
+			free(buf);
+			close(fd);
+			return (0);
+		}
+		total_written += w;
+	}
+free(buf);
+close(fd);
+return (total_written);
+}
